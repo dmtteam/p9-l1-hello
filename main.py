439c2e1c43b0f289dc187e9d226f24f02760e3bc
Marcin T <yahoospider@gmail.com>
2022-07-27 14:38:54 +0200

    first year shortcuts translation

--- main.py
+++ main.py
@@ -37,46 +37,46 @@ m12r2=1.499708521
 left = "The remaining loan amount is:"
 less = "less than the previous month"
 
-sty1=round((1 + ((m1r1+interest)/1200)) * loanamount - rate, 2)
-print((left), str(sty1), ",wchich is:", round((loanamount - sty1), 2), (less))
+jan1=round((1 + ((m1r1+interest)/1200)) * loanamount - rate, 2)
+print((left), str(jan1), ",wchich is:", round((loanamount - jan1), 2), (less))
 
-lut1=round((1 + ((m2r1+interest)/1200)) * sty1 - rate, 2)
-print((left), str(lut1), ",wchich is:", round((sty1 -lut1), 2), (less))
+feb1=round((1 + ((m2r1+interest)/1200)) * jan1 - rate, 2)
+print((left), str(feb1), ",wchich is:", round((jan1 -feb1), 2), (less))
 
-mar1=round((1 + ((m3r1+interest)/1200)) * lut1 - rate, 2)
-print((left), str(mar1), ",wchich is:", round((lut1 -mar1), 2), (less))
+mar1=round((1 + ((m3r1+interest)/1200)) * feb1 - rate, 2)
+print((left), str(mar1), ",wchich is:", round((feb1 -mar1), 2), (less))
 
-kwi1=round((1 + ((m4r1+interest)/1200)) * mar1 - rate, 2)
-print((left), str(kwi1), ",wchich is:", round((mar1 -kwi1), 2), (less))
+apr1=round((1 + ((m4r1+interest)/1200)) * mar1 - rate, 2)
+print((left), str(apr1), ",wchich is:", round((mar1 -apr1), 2), (less))
 
-maj1=round((1 + ((m5r1+interest)/1200)) * kwi1 - rate, 2)
-print((left), str(maj1), ",wchich is:", round((kwi1 -maj1), 2), (less))
+may1=round((1 + ((m5r1+interest)/1200)) * apr1 - rate, 2)
+print((left), str(may1), ",wchich is:", round((apr1 -may1), 2), (less))
 
-cze1=round((1 + ((m6r1+interest)/1200)) * maj1 - rate, 2)
-print((left), str(cze1), ",wchich is:", round((maj1 -cze1), 2), (less))
+jun1=round((1 + ((m6r1+interest)/1200)) * may1 - rate, 2)
+print((left), str(jun1), ",wchich is:", round((may1 -jun1), 2), (less))
 
-lip1=round((1 + ((m7r1+interest)/1200)) * cze1 - rate, 2)
-print((left), str(lip1), ",wchich is:", round((cze1 -lip1), 2), (less))
+jul1=round((1 + ((m7r1+interest)/1200)) * jun1 - rate, 2)
+print((left), str(jul1), ",wchich is:", round((jun1 -jul1), 2), (less))
 
-sie1=round((1 + ((m8r1+interest)/1200)) * lip1 - rate, 2)
-print((left), str(sie1), ",wchich is:", round((lip1 -sie1), 2), (less))
+aug1=round((1 + ((m8r1+interest)/1200)) * jul1 - rate, 2)
+print((left), str(aug1), ",wchich is:", round((jul1 -aug1), 2), (less))
 
-wrz1=round((1 + ((m9r1+interest)/1200)) * sie1 - rate, 2)
-print((left), str(wrz1), ",wchich is:", round((sie1 -wrz1), 2), (less))
+sep1=round((1 + ((m9r1+interest)/1200)) * aug1 - rate, 2)
+print((left), str(sep1), ",wchich is:", round((aug1 -sep1), 2), (less))
 
-paz1=round((1 + ((m10r1+interest)/1200)) * wrz1 - rate, 2)
-print((left), str(paz1), ",wchich is:", round((wrz1 -paz1), 2), (less))
+oct1=round((1 + ((m10r1+interest)/1200)) * sep1 - rate, 2)
+print((left), str(oct1), ",wchich is:", round((sep1 -oct1), 2), (less))
 
-lis1=round((1 + ((m11r1+interest)/1200)) * paz1 - rate, 2)
-print((left), str(lis1), ",wchich is:", round((paz1 -lis1), 2), (less))
+nov1=round((1 + ((m11r1+interest)/1200)) * sep1 - rate, 2)
+print((left), str(nov1), ",wchich is:", round((sep1 -nov1), 2), (less))
 
-gru1=round((1 + ((m12r1+interest)/1200)) * lis1 - rate, 2)
-print((left), str(gru1), ",wchich is:", round((lis1 -gru1), 2), (less))
+dec1=round((1 + ((m12r1+interest)/1200)) * nov1 - rate, 2)
+print((left), str(dec1), ",wchich is:", round((nov1 -dec1), 2), (less))
 
 # second year
 
-sty2=round((1 + ((m1r2+interest)/1200)) * gru1 - rate, 2)
-print((left), str(sty2), ",wchich is:", round((gru1 - sty2), 2), (less))
+sty2=round((1 + ((m1r2+interest)/1200)) * dec1 - rate, 2)
+print((left), str(sty2), ",wchich is:", round((dec1 - sty2), 2), (less))
 
 lut2=round((1 + ((m2r2+interest)/1200)) * sty2 - rate, 2)
 print((left), str(lut2), ",wchich is:", round((sty2 -lut2), 2), (less))
@@ -105,10 +105,10 @@ print((left), str(wrz2), ",wchich is:", round((sie2 -wrz2), 2), (less))
 paz2=round((1 + ((m10r2+interest)/1200)) * wrz2 - rate, 2)
 print((left), str(paz2), ",wchich is:", round((wrz2 -paz2), 2), (less))
 
-lis2=round((1 + ((m11r2+interest)/1200)) * paz2 - rate, 2)
-print((left), str(lis2), ",wchich is:", round((paz2 -lis2), 2), (less))
+nov2=round((1 + ((m11r2+interest)/1200)) * paz2 - rate, 2)
+print((left), str(nov2), ",wchich is:", round((paz2 -nov2), 2), (less))
 
-gru2=round((1 + ((m12r2+interest)/1200)) * lis2 - rate, 2)
-print((left), str(gru2), ",wchich is:", round((lis2 -gru2), 2), (less))
+dec2=round((1 + ((m12r2+interest)/1200)) * nov2 - rate, 2)
+print((left), str(dec2), ",wchich is:", round((nov2 -dec2), 2), (less))
 
 
